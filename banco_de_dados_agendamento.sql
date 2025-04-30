fdc6ee2e80eac61c10c3f5bde59ace695c2d787d
Adao-gb <adaosnts077@gmail.com>
2025-04-30 10:56:54 -0300

    Update banco_de_dados_agendamento.sql
    ADICIONADO TABELA ADMIN

--- banco_de_dados_agendamento.sql
+++ banco_de_dados_agendamento.sql
@@ -42,3 +42,10 @@ CREATE TABLE agendamento_equipamentos (
     FOREIGN KEY (agendamento_id) REFERENCES agendamentos(id),
     FOREIGN KEY (equipamento_id) REFERENCES equipamentos(id)
 );
+
+CREATE TABLE admins (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);
